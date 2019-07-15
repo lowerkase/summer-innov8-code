a18b05ee762f2c2f48c993990aab94923d93c77e
Kasey <kaseyicecream@gmail.com>
2019-07-15 13:34:30 -0400

    Added scanners to each function so the user can imput words or numbers, all of them except "compare" work

--- Homework3.java
+++ Homework3.java
@@ -1,26 +1,37 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Homework3 {
 
 
     // Function 1
-    public static void capsLock (String str) {
+    public static void capsLock () {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter a word or phrase");
+        String str = scan.nextLine();
         System.out.println(str.toUpperCase());
     }
 
     //Function 2
-    public static int wordCount (String str) {
+    public static int wordCount () {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter a word or phrase");
+        String str = scan.nextLine();
         String[] words = str.split(" ");
         System.out.println(words.length);           
         return words.length;
     }
 
     //Function 3
-    public static int highestInt (int things[]) {
-        int x = 0;
+    public static int highestInt () {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter some numbers, separated by commas");
+        String str = scan.nextLine();
+        String things[] = str.split(",");
+        int x = Integer.parseInt(things[0]);
         for (int i = things.length; i > 0; i--) {
-            if (things[i-1] > x) {
-                x = things[i-1];
+            if (Integer.parseInt(things[i-1]) > x) {
+                x = Integer.parseInt(things[i-1]);
             }
         }
         System.out.println(x);
@@ -28,10 +39,14 @@ public class Homework3 {
     }
 
     //Function 4
-    public static int meanInt (int things[]) {
+    public static int meanInt () {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter some numbers, separated by commas");
+        String str = scan.nextLine();
+        String things[] = str.split(",");
         int x = 0;
         for (int i = things.length; i > 0; i--) {
-            x = x + things[i-1];
+            x = x + Integer.parseInt(things[i-1]);
         }
         x = x/things.length;
         System.out.println(x);
@@ -39,31 +54,48 @@ public class Homework3 {
     }
 
     //Function 5
-    public static int[] sevenTwelve (int things[]) {
-        for (int i = things.length; i > 0; i--) {
-            if (things[i-1] == 7) {
-                things[i-1] = 12;
+    public static String[] sevenTwelve () {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter some numbers, separated by commas");
+        String str = scan.nextLine();
+        String things[] = str.split(",");
+        for (int i = 1; i <= things.length; i++) {
+            if (Integer.parseInt(things[i-1]) == 7) {
+                System.out.print("12, ");
             }
+            else System.out.print(Integer.parseInt(things[i-1])+", ");
         }
-        System.out.println(things);
+        System.out.println(" ");
         return things;
     }
 
     //Function 6
-    public static String reverse (String str) {
+    public static String reverse () {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter a word or phrase");
+        String str = scan.nextLine();
         String[] words = str.split(" ");
         int x = 0;
-        int y[] = new int[words.length];
+        String y[] = new String[words.length];
         for (int i = words.length; i > 0; i--) {
             y[x] = words[i-1];
             x = x+1;
         }
+        
         System.out.println(Arrays.toString(y));
         return Arrays.toString(y);
     }
 
     //Function 7
-    public static void compare (int things[],int stuff[]) {
+    public static void compare () {
+        Scanner thing = new Scanner (System.in);
+        System.out.println("Enter some numbers, separated by commas");
+        String strUno = thing.nextLine();
+        String things[] = strUno.split(",");
+        Scanner stuffs = new Scanner (System.in);
+        System.out.println("Enter some more numbers, separated by commas");
+        String strDos = stuffs.nextLine();
+        String stuff[] = strDos.split(",");
         if (things.length != stuff.length) {
             System.out.println("The arrays are not identical");
             return;
@@ -78,7 +110,13 @@ public class Homework3 {
     }
 
     public static void main (String args[]) {
+
         
+        compare();
+
+        //Scanner scan = new Scanner (System.in);
+        //System.out.println("Enter a number");
+        //int x = scan.nextInt();
 
 
     }
